699eba34979a971fec8a7bcbafcb51d738709374
Angel <davidserrano14545@gmail.com>
2025-09-14 22:20:15 +0530

    Versión final del portafolio

---
+++ js/index.js
@@ -0,0 +1,146 @@
+// --------------- Abrir Formulario de contacto ---------------
+function abrirFormularioContacto() {
+    const abrirFormulario = document.getElementById('formulario-contacto')
+    abrirFormulario.style.display = 'block'
+}
+
+
+// --------------- Cerrar Formulario de contacto ---------------
+function cerrarFormularioContacto() {
+    const cerrarFormulario = document.getElementById('formulario-contacto')
+    cerrarFormulario.style.display = 'none'
+}
+
+
+/*
+---------------------------------------
+    VENTANA DEL PROYECTO A.D.C.SUITE
+---------------------------------------
+*/
+
+// --------------- Abrir Ventana emergente del proyecto A.D.C.Suite ---------------
+function abrirProyectoAdcsuite() {
+    const abrirAdcsuite = document.getElementById('adc-suite-modal')
+    abrirAdcsuite.style.display = 'flex'
+}
+
+
+// --------------- Cerrar Ventana emergente del proyecto A.D.C.Suite ---------------
+function cerrarProyectoAdcsuite() {
+    const cerrarAdcsuite = document.getElementById('adc-suite-modal')
+    cerrarAdcsuite.style.display = 'none'
+}
+
+
+/*
+----------------------------------------------
+    VENTANA DEL PROYECTO THE BURGER HOUSE
+----------------------------------------------
+*/
+
+// --------------- Abrir Ventana emergente del proyecto The Burger House ---------------
+function abrirProyectoBurger() {
+    const abrirBurger = document.getElementById('the-burger-house-modal')
+    abrirBurger.style.display = 'flex'
+}
+
+// --------------- Cerrar Ventana emergente del proyecto The Burger House ---------------
+function cerrarProyectoBurger() {
+    const cerrarBurger = document.getElementById('the-burger-house-modal')
+    cerrarBurger.style.display = 'none'
+}
+
+
+/*
+----------------------------------------------
+    VENTANA DEL PROYECTO WARRSPA
+----------------------------------------------
+*/
+
+// --------------- Abrir Ventana emergente del proyecto WarrSpa ---------------
+function abrirProyectoWarrspa() {
+    const abrirWarrspa = document.getElementById('warr-spa-modal')
+    abrirWarrspa.style.display = 'flex'
+}
+
+// --------------- Cerrar Ventana emergente del proyecto WarrSpa ---------------
+function cerrarProyectoWarrspa() {
+    const cerrarWarrspa = document.getElementById('warr-spa-modal')
+    cerrarWarrspa.style.display = 'none'
+}
+
+
+/* 
+----------------------------------------------
+    VENTANA DEL PROYECTO ROBERTO FLAX
+----------------------------------------------
+*/
+
+// Abrir
+function abrirProyectoRobertoFlax() {
+    const abrirFlax = document.getElementById('roberto-flax-modal');
+    abrirFlax.style.display = 'flex';
+}
+
+// Cerrar
+function cerrarProyectoRobertoFlax() {
+    const cerrarFlax = document.getElementById('roberto-flax-modal');
+    cerrarFlax.style.display = 'none';
+}
+
+
+/* 
+----------------------------------------------
+    VENTANA DEL PROYECTO THE BURGER HOUSE ADM
+----------------------------------------------
+*/
+
+// Abrir
+function abrirProyectoBurgerAdm() {
+    const abrirBurgerAdm = document.getElementById('the-burger-house-adm-modal');
+    abrirBurgerAdm.style.display = 'flex';
+}
+
+// Cerrar
+function cerrarProyectoBurgerAdm() {
+    const cerrarBurgerAdm = document.getElementById('the-burger-house-adm-modal');
+    cerrarBurgerAdm.style.display = 'none';
+}
+
+
+/* 
+----------------------------------------------
+    VENTANA DEL PROYECTO DENTAL CROWN
+----------------------------------------------
+*/
+
+// Abrir
+function abrirProyectoDentalCrown() {
+    const abrirDental = document.getElementById('dental-crown-modal');
+    abrirDental.style.display = 'flex';
+}
+
+// Cerrar
+function cerrarProyectoDentalCrown() {
+    const cerrarDental = document.getElementById('dental-crown-modal');
+    cerrarDental.style.display = 'none';
+}
+
+
+/* 
+----------------------------------------------
+    VENTANA DEL PROYECTO MY BLOG
+----------------------------------------------
+*/
+
+// Abrir
+function abrirProyectoMyBlog() {
+    const abrirBlog = document.getElementById('my-blog-modal');
+    abrirBlog.style.display = 'flex';
+}
+
+// Cerrar
+function cerrarProyectoMyBlog() {
+    const cerrarBlog = document.getElementById('my-blog-modal');
+    cerrarBlog.style.display = 'none';
+}
